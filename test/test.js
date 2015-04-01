33be1ae6c353ceb0252a1bef1e6f8ad6d98a9da8
Tuur Dutoit <me@tuurdutoit.be>
2015-04-01 17:25:12 +0200

    Restructured tests

--- test/test.js
+++ test/test.js
@@ -6,64 +6,114 @@ var Crash = require("../src/index.js");
 describe("Crash", function() {
     
     describe("attributes", function() {
-        it("should expose RBush", function() {
-            expect(Crash.RBush).to.be.ok();
-            expect(Crash.RBush).to.be.a("function");
+        
+        describe("RBush", function() {
+            it("should be defined", function() {
+                expect(Crash.RBush).to.be.ok();
+            });
+            it("should be a function", function() {
+                expect(Crash.RBush).to.be.a("function");
+            });
         });
         
-        it("should expose SAT", function() {
-            expect(Crash.SAT).to.be.ok();
-            expect(Crash.SAT).to.be.an("object");
+        describe("SAT", function() {
+            it("should be defined", function() {
+                expect(Crash.SAT).to.be.ok();
+            });
+            it("should be an object", function() {
+                expect(Crash.SAT).to.be.an("object");
+            });
         });
         
-        it("should expose Vector, which should equal SAT.Vector", function() {
-            expect(Crash.Vector).to.be.ok();
-            expect(Crash.Vector).to.equal(Crash.SAT.Vector);
+        describe("Vector", function() {
+            it("should be defined", function() {
+                expect(Crash.Vector).to.be.ok();
+            });
+            it("should equal SAT.Vector", function() {
+                expect(Crash.Vector).to.equal(Crash.SAT.Vector);
+            });
         });
         
-        it("should expose V, which should equal SAT.Vector", function() {
-            expect(Crash.V).to.be.ok();
-            expect(Crash.V).to.equal(Crash.SAT.Vector);
+        describe("V", function() {
+            it("should be defined", function() {
+                expect(Crash.V).to.be.ok();
+            });
+            it("should equal SAT.Vector", function() {
+                expect(Crash.V).to.equal(Crash.SAT.Vector);
+            });
         });
         
-        it("should expose Response, which should equal SAT.Response", function() {
-            expect(Crash.Response).to.be.ok();
-            expect(Crash.Response).to.equal(Crash.SAT.Response);
+        describe("Response", function() {
+            it("should be defined", function() {
+                expect(Crash.Response).to.be.ok();
+            });
+            it("should equal SAT.Response", function() {
+                expect(Crash.Response).to.equal(Crash.SAT.Response);
+            });
         });
         
-        it("should expose a cancel() function", function() {
-            expect(Crash.cancel).to.be.ok();
-            expect(Crash.cancel).to.be.a("function");
+        describe("cancel", function() {
+            it("should be defined", function() {
+                expect(Crash.cancel).to.be.ok();
+            });
+            it("should be a function", function() {
+                expect(Crash.cancel).to.be.a("function");
+            });
         });
         
-        it("should expose maxChecks, which should be 100 by default", function() {
-            expect(Crash.maxChecks).to.be.ok();
-            expect(Crash.maxChecks).to.be.a("number");
-            expect(Crash.maxChecks).to.equal(100);
+        describe("maxChecks", function() {
+            it("should be defined", function() {
+                expect(Crash.maxChecks).to.be.ok();
+            });
+            it("should be a number", function() {
+                expect(Crash.maxChecks).to.be.a("number");
+            });
+            it("should equal 100 (default)", function() {
+                expect(Crash.maxChecks).to.equal(100);
+            });
         });
         
-        it("should expose RESPONSE, which should be a Response", function() {
-            expect(Crash.RESPONSE).to.be.ok();
-            expect(Crash.RESPONSE).to.be.a(Crash.Response);
+        
+        describe("RESPONSE", function() {
+            it("should be defined", function() {
+                expect(Crash.RESPONSE).to.be.ok();
+            });
+            it("should be a Response", function() {
+                expect(Crash.RESPONSE).to.be.a(Crash.Response);
+            });
         });
         
-        it("should expose onCollision", function() {
-            expect(Crash.onCollision).to.be.ok();
-            expect(Crash.onCollision).to.be.a("function");
+        describe("onCollision", function() {
+            it("should expose onCollision", function() {
+                expect(Crash.onCollision).to.be.ok();
+            });
+            it("should be a function", function() {
+                expect(Crash.onCollision).to.be.a("function");
+            });
         });
         
-        it("should expose rbush, which is null by default", function() {
-            expect(Crash.rbush).to.be(null);
+        describe("rbush", function() {
+            it("should be null (default)", function() {
+                expect(Crash.rbush).to.be(null);
+            }); 
         });
         
-        it("should expose __notYetInserted, which should be an Array", function() {
-            expect(Crash.__notYetInserted).to.be.ok();
-            expect(Crash.__notYetInserted).to.be.an("array");
+        describe("__notYetInserted", function() {
+            it("should be defined", function() {
+                expect(Crash.__notYetInserted).to.be.ok();
+            });
+            it("should be an array", function() {
+                expect(Crash.__notYetInserted).to.be.an("array");
+            });
         });
         
-        it("should expose __moved, which should be an Array", function() {
-            expect(Crash.__moved).to.be.ok();
-            expect(Crash.__moved).to.be.an("array");
+        describe("__moved", function() {
+            it("should be defined", function() {
+                expect(Crash.__moved).to.be.ok();
+            });
+            it("should be an array", function() {
+                expect(Crash.__moved).to.be.an("array");
+            });
         });
     });
     
